9eaea950e3ec54d8676062094e13b7692561538e
JuniorCorzo <josedanielmmf@gmail.con>
2026-05-23 16:51:48 -0500

    feat(procurement-notices): expose manual classification API endpoint

--- apps/nest/src/modules/procurement-notices/procurement-notices.module.ts
+++ apps/nest/src/modules/procurement-notices/procurement-notices.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IngestionJob } from './entities/ingestion-job.entity';
 import { ProcurementNotice } from './entities/procurement-notice.entity';
+import { SectorKeyword } from './entities/sector-keyword.entity';
 import { ProcurementNoticesService } from './services/procurement-notices.service';
 import { ProcurementIngestionService } from './services/ingestion.service';
+import { SectorClassifierService } from './services/sector-classifier.service';
 import { ProcurementNoticesController } from './controllers/procurement-notices.controller';
 import { QueuesModule } from '../queues/queues.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProcurementNotice, IngestionJob]), QueuesModule],
+  imports: [TypeOrmModule.forFeature([ProcurementNotice, IngestionJob, SectorKeyword]), QueuesModule],
   controllers: [ProcurementNoticesController],
-  providers: [ProcurementNoticesService, ProcurementIngestionService],
+  providers: [ProcurementNoticesService, ProcurementIngestionService, SectorClassifierService],
   exports: [ProcurementNoticesService],
 })
 export class ProcurementNoticesModule {}
+
